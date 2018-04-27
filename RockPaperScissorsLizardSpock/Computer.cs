387f00508f8c92d72313773a19b389d9ee572958
Jacob Hansen <hansenjacobs@gmail.com>
2018-04-27 10:01:30 -0500

    setup PLayer class

--- RockPaperScissorsLizardSpock/Computer.cs
+++ RockPaperScissorsLizardSpock/Computer.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace RockPaperScissorsLizardSpock
 {
-    class Computer
+    class Computer : Player
     {
+        
     }
 }
--- RockPaperScissorsLizardSpock/Player.cs
+++ RockPaperScissorsLizardSpock/Player.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace RockPaperScissorsLizardSpock
 {
-    class Player
+    abstract class Player
     {
+        string name;
+        int roundsWon;
+
+        public Player(string name)
+        {
+            this.name = name;
+            roundsWon = 0;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public int GetRoundsWon()
+        {
+            return roundsWon;
+        }
+
+        public abstract Gesture GetRPSLSChoice(List<Gesture> gestures);
     }
 }
